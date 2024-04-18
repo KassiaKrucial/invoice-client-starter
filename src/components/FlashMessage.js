1aedeed62a505670898a43f02440b03fae1ab69f
Kat <klicova.katerina@seznam.cz>
2024-04-19 01:52:10 +0200

    Jsdoc for FlashMessage.js added

--- src/components/FlashMessage.js
+++ src/components/FlashMessage.js
@@ -1,5 +1,13 @@
 import React from "react";
 
+/**
+ * Creates a flash message component
+ * @component
+ * @param theme Color of background in bootstrap
+ * @param text Text of the message
+ * @returns {JSX.Element} The rendered flash message
+ * @constructor makes an instance of flash message
+ */
 export function FlashMessage({ theme, text }) {
   return <div className={"alert alert-" + theme}>{text}</div>;
 }
