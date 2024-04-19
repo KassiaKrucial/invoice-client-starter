acf8323fa467594894853aeaf571bec824731697
Kat <klicova.katerina@seznam.cz>
2024-04-19 22:06:20 +0200

    Jsdoc for StatisticsIndex.js added

--- src/statistics/StatisticsIndex.js
+++ src/statistics/StatisticsIndex.js
@@ -2,10 +2,20 @@ import {useEffect, useState} from "react";
 import {apiGet} from "../utils/api";
 import PersonStatisticsTable from "./PersonStatisticsTable";
 
-
+/**
+ * Fetches data for statistics of specific people/companies and sends into {@link PersonStatisticsTable}
+ * @returns {JSX.Element} Renders the table with mapped data and a <h1>
+ * @constructor
+ */
 const StatisticsIndex = () => {
+    /**
+     * Contains statistics data - person's/company's id and name, their sum of revenue
+     */
     const [personStatisticsState, setPersonStatistics] = useState([]);
 
+    /**
+     * Fetches data from server and saves them into {@link personStatisticsState}
+     */
     useEffect(() => {
         apiGet("/api/persons/statistics").then((data) => setPersonStatistics(data));
     }, []);
