8687d52db33ba649e5ed4c5b2a2b298826aa7719
Kat <klicova.katerina@seznam.cz>
2024-04-19 22:08:34 +0200

    Jsdoc for CZMoneyFormatter.js added

--- src/utils/CZMoneyFormatter.js
+++ src/utils/CZMoneyFormatter.js
@@ -1,3 +1,7 @@
+/**
+ * Formats number into: "d ddd,dd Kč" (d...digit)
+ * @type {Intl.NumberFormat}
+ */
 export const CZMoney = Intl.NumberFormat('cs-CZ', {
     style: 'currency',
     currency: 'CZK',
